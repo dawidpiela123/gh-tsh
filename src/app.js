12b0d60dffd19fbeef617b230b925ec49de85c46
dpiela <d.piela@fireup.pro>
2020-05-26 23:19:08 +0200

    fix-1/data loading on all browsers

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import './assets/scss/app.scss';
 import $ from 'cash-dom';
+import { polyfill } from 'es6-promise';
+import 'isomorphic-fetch';
 
+polyfill();
 
 export class App {
   initializeApp() {
@@ -10,20 +13,20 @@ export class App {
       let userName = $('.username.input').val();
 
       fetch('https://api.github.com/users/' + userName)
-        .then((response)=> {response.json})
+        .then((response) => response.json())
         .then(function (body) {
           self.profile = body;
           self.update_profile();
-        })
-
-    })
-
+        });
+    });
   }
 
   update_profile() {
-    $('#profile-name').text($('.username.input').val())
-    $('#profile-image').attr('src', this.profile.avatar_url)
-    $('#profile-url').attr('href', this.profile.html_url).text(this.profile.login)
-    $('#profile-bio').text(this.profile.bio || '(no information)')
+    $('#profile-name').text($('.username.input').val());
+    $('#profile-image').attr('src', this.profile.avatar_url);
+    $('#profile-url')
+      .attr('href', this.profile.html_url)
+      .text(this.profile.login);
+    $('#profile-bio').text(this.profile.bio || '(no information)');
   }
 }
